9444e98b5cbd60c7144e12cdfbd6210961b40ebf
Hanna <hweissbe@g.jct.ac.il>
2018-11-25 21:34:03 +0200

    added genarete

---
+++ app/src/main/java/com/libby/hanna/drivingalltheway/controller/TravelApp.java
@@ -0,0 +1,16 @@
+package com.libby.hanna.drivingalltheway.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.libby.hanna.drivingalltheway.R;
+
+public class TravelApp extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_travel_app);
+
+    }
+}
