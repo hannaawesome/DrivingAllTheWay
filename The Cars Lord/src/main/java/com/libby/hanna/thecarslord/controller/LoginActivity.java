b6a371fda5919a3011884460d5cba5d943025de3
Hanna <hweissbe@g.jct.ac.il>
2019-01-13 02:00:12 +0200

    still go issues

--- The Cars Lord/src/main/java/com/libby/hanna/thecarslord/controller/LoginActivity.java
+++ The Cars Lord/src/main/java/com/libby/hanna/thecarslord/controller/LoginActivity.java
@@ -1,16 +1,25 @@
 package com.libby.hanna.thecarslord.controller;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,12 +30,13 @@ import com.google.firebase.auth.FirebaseUser;
 import com.libby.hanna.thecarslord.R;
 import com.libby.hanna.thecarslord.model.entities.Driver;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
     private EditText name;
     private EditText password;
     private FirebaseAuth userAuth;
     FirebaseUser currentUser;
-    private AppCompatButton register;
+    private AppCompatButton log;
+    private TextView reg;
     private CheckBox remember;
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
@@ -39,16 +49,50 @@ public class LoginActivity extends AppCompatActivity {
         password = (EditText) findViewById(R.id.editTextUserPassword);
         remember = (CheckBox) findViewById(R.id.saveLoginCheckBox);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        userAuth = FirebaseAuth.getInstance();
-        register = (AppCompatButton) findViewById(R.id.register);
-        register.setOnClickListener(new View.OnClickListener() {
+        // userAuth = FirebaseAuth.getInstance();
+        loadSharedPreferences();
+        log = (AppCompatButton) findViewById(R.id.login);
+        log.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), RegisterActivity.class);
-                startActivity(intent);
+                if(validations()){
+                saveSharedPrefences();
+                logIn(name.getText().toString(),password.getText().toString());
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);}
             }
         });
+        SpannableString ss = new SpannableString("No account yet? Create one");
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View textView) {
+                startActivity(new Intent(getBaseContext(), RegisterActivity.class));
+            }
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(false);
+            }
+        };
+        ss.setSpan(clickableSpan, 16, 26, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        reg = (TextView) findViewById(R.id.link_signup);
+        reg.setText(ss);
+        reg.setMovementMethod(LinkMovementMethod.getInstance());
+        reg.setHighlightColor(Color.TRANSPARENT);
     }
-
+    private boolean validations() {
+        boolean check=true;
+        String strEmail = name.getText().toString();
+        String strPassword = password.getText().toString();
+        if (TextUtils.isEmpty(strEmail)) {
+            name.setError("Email must be entered");
+            check = false;
+        }
+        if (TextUtils.isEmpty(strPassword)) {
+            password.setError("password must be entered");
+            check = false;
+        }
+        return check;
+    }
     private boolean loadSharedPreferences() {
         if (sharedPreferences.contains("SavePass"))
             if (!sharedPreferences.getBoolean("SavePass", false))
