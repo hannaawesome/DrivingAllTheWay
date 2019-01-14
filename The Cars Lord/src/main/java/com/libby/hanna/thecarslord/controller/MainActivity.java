f831c9552cc09db7978fdd3d786279a96e07dd27
itsmelibbyo <33591971+itsmelibbyo@users.noreply.github.com>
2019-01-14 13:44:15 +0200

    push weird funcs

--- The Cars Lord/src/main/java/com/libby/hanna/thecarslord/controller/MainActivity.java
+++ The Cars Lord/src/main/java/com/libby/hanna/thecarslord/controller/MainActivity.java
@@ -1,5 +1,117 @@
 package com.libby.hanna.thecarslord.controller;
 
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.libby.hanna.thecarslord.R;
+import com.libby.hanna.thecarslord.model.backend.DB_manager;
+import com.libby.hanna.thecarslord.model.datasource.Firebase_DBManager;
+import com.libby.hanna.thecarslord.model.entities.Driver;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+    private DB_manager be;
+    private FirebaseAuth userAuth;
+    private FirebaseUser currentUser;
+    private Driver driver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dl = (DrawerLayout)findViewById(R.id.activity_main);
+        t = new ActionBarDrawerToggle(this, dl, 0, R.string.app_name);
+
+        dl.addDrawerListener(t);
+        t.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = (NavigationView)findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                switch (id) {
+                    case R.id.tripsOnWait:
+                        loadFragment(new FirstFragment());
+                        return true;
+                    case R.id.myTips:
+                        loadFragment(new SecondFragment());
+                        return true;
+                    case R.id.exit:
+                        signOut();
+                        finish();
+                        System.exit(0);
+                    default:
+                        return true;
+                }
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void loadFragment(Fragment fragment) {
+        dl.closeDrawer(nv);
+        // create a FragmentManager
+        FragmentManager fm = getFragmentManager();
+        // create a FragmentTransaction to begin the transaction and replace the Fragment
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        // replace the FrameLayout with new Fragment
+        fragmentTransaction.replace(R.id.relativeLay, fragment);
+        fragmentTransaction.commit(); // save the changes
+    }
+
+    protected void onDestroy() {
+        Firebase_DBManager.stopNotifyToTripList();
+        super.onDestroy();
+    }
+
+    private void signOut() {
+        userAuth.signOut();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*
+package com.libby.hanna.thecarslord.controller;
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -60,8 +172,8 @@ public class MainActivity extends AppCompatActivity {
                /* List<Trip> d = be.getTripsByPrice();
                 if (d != null || d.size() != 0)
                     done.setText(d.get(0).getEmailAddress());*/
-            }
 
+/*
             @Override
             public void onFailure(Exception exception) {
                 Toast.makeText(getBaseContext(), "error to get trips list\n" + exception.toString(), Toast.LENGTH_LONG).show();
@@ -113,27 +225,13 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void loadFragment(Fragment fragment) {
-        dl.closeDrawer(nv);
-        // create a FragmentManager
-        FragmentManager fm = getFragmentManager();
-        // create a FragmentTransaction to begin the transaction and replace the Fragment
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-        // replace the FrameLayout with new Fragment
-        fragmentTransaction.replace(R.id.frameLayout, fragment);
-        fragmentTransaction.commit(); // save the changes
-    }
+
 
 
     /*private void loadDataOnCurrentDriver(){
         String id=currentUser.getUid();
 }*/
-    protected void onDestroy() {
-        Firebase_DBManager.stopNotifyToTripList();
-        super.onDestroy();
-    }
+/*
 
-    private void signOut() {
-        userAuth.signOut();
-    }
 }
+*/
